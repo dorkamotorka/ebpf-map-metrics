7356a84f0e0ad567d6554913f84a4528b324e96f
Teo Podobnik <teo.podobnik1234@gmail.com>
2024-10-19 09:10:57 +0000

    format code

--- src/count.c
+++ src/count.c
@@ -1,4 +1,4 @@
-//go:build ignore
+// go:build ignore
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 
@@ -6,19 +6,18 @@ __s64 bpf_map_sum_elem_count(struct bpf_map *map) __ksym;
 
 SEC("iter/bpf_map")
 int dump_bpf_map(struct bpf_iter__bpf_map *ctx) {
-    struct seq_file *seq = ctx->meta->seq;
-    __u64 seq_num = ctx->meta->seq_num;
-    struct bpf_map *map = ctx->map;
+  struct seq_file *seq = ctx->meta->seq;
+  __u64 seq_num = ctx->meta->seq_num;
+  struct bpf_map *map = ctx->map;
 
-    if (!map) {
-        return 0;
-    }
-
-    BPF_SEQ_PRINTF(seq, "%4u %-16s %10d %10lld\n",
-                   map->id, map->name, map->max_entries,
-                   bpf_map_sum_elem_count(map));
-
+  if (!map) {
     return 0;
+  }
+
+  BPF_SEQ_PRINTF(seq, "%4u %-16s %10d %10lld\n", map->id, map->name,
+                 map->max_entries, bpf_map_sum_elem_count(map));
+
+  return 0;
 }
 
 char _license[] SEC("license") = "GPL";
